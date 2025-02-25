2d41e9772de0a4c2941658aaa4738ba7b9b5f89b
smin0820 <97029953+smin0820@users.noreply.github.com>
2025-02-25 21:36:22 +0900

    style: Tailwind로 스타일 변경

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,32 @@
-import Layout from "@/component/Layout";
 import Link from "next/link";
-
 
 export default function Home() {
   return (
-    <Layout>
-      <h1>Mapa Index Page</h1>
-      <ul>
-        <li><Link href="/stores">맛집 목록</Link></li>
-        <li><Link href="/stores/new">맛집 생성</Link></li>
-        <li><Link href="/stores/1">맛집 상세 페이지</Link></li>
-        <li><Link href="/stores/1/edit">맛집 수정 페이지</Link></li>
-        <li><Link href="/users/login">로그인 페이지</Link></li>
-        <li><Link href="/users/mypage">마이 페이지</Link></li>
-        <li><Link href="/users/likes">찜한 맛집</Link></li>
+    <>
+      <h1 className="font-bold">Mapa Index Page</h1>
+      <ul className="px-4 py-2">
+        <li>
+          <Link href="/stores">맛집 목록</Link>
+        </li>
+        <li>
+          <Link href="/stores/new">맛집 생성</Link>
+        </li>
+        <li>
+          <Link href="/stores/1">맛집 상세 페이지</Link>
+        </li>
+        <li>
+          <Link href="/stores/1/edit">맛집 수정 페이지</Link>
+        </li>
+        <li>
+          <Link href="/users/login">로그인 페이지</Link>
+        </li>
+        <li>
+          <Link href="/users/mypage">마이 페이지</Link>
+        </li>
+        <li>
+          <Link href="/users/likes">찜한 맛집</Link>
+        </li>
       </ul>
-    </Layout>
+    </>
   );
 }
